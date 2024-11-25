eb737e5638f96c51bdbd50ca71376b050d188251
Johnkennedy <muhumuzakennedy17@gmail.com>
2024-11-25 19:33:37 +0300

    add search icon

--- app/components/shared/layout/Header.tsx
+++ app/components/shared/layout/Header.tsx
@@ -25,12 +25,22 @@ export const Header = () => {
           <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z" />
         </svg>
       </span>
-      <form>
+      <form className=" relative">
         <input
           type="text"
           placeholder="Search Tuliime"
-          className="rounded-md px-2 placeholder:text-stone-400 text-zinc-700 outline-none"
+          className="rounded-md px-4 py-2  text-zinc-700 outline-none text-sm placeholder:text-gray-500"
         />
+        <span className=" absolute top-2 right-1 text-gray-500">
+          <svg
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            height="1.5em"
+            width="1.5em"
+          >
+            <path d="M10 18a7.952 7.952 0 004.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0018 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z" />
+          </svg>
+        </span>
       </form>
       <div className="flex gap-4">
         <span>EN</span>
